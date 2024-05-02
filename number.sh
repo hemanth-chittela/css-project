4a1713fce23bbb56206228fc3cb11f1c717ab734
hemanth-chittela <114971743+hemanth-chittela@users.noreply.github.com>
2024-05-02 17:53:09 +0530

    Create number.sh

---
+++ number.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo 'printing even numbers'
+
+for ((i=1; i<=10; i++))
+do
+        if((i%2==0))
+then
+        echo $i
+   fi
+done
+
+
+echo 'printing odd numbers'
+
+for ((i=1; i<=10; i++))
+do
+        if((i%2!=0))
+then
+        echo $i
+   fi
+done
